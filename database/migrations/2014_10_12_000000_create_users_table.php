f24ee273032d49b554451d3a59a1c781005fc6cb
Houcem Hedhly <hedhoucem@gmail.com>
2021-07-06 15:27:44 +0100

    udate users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('address', 100);
+            $table->string('phone');
+            $table->boolean('admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
